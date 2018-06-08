28e841d3305029bde8641146b8881901091a516b
cmpablo <christine.pablo@gmail.com>
2018-06-07 22:11:13 -0400

    updated files for heroku deployment

--- server.js
+++ server.js
@@ -8,13 +8,12 @@ const path = require("path");
 const app = express();
 const router = require ('./routes/router.js');
 const cors = require ('cors');
+const PORT = process.env.PORT || 3090;
 
 // ===== db setup =====
 mongoose.connect(
-  process.envMONGODB_URI || 'mongodb://localhost/auth',
-{
-  useMongoClient: true
-});
+  process.env.MONGODB_URI || 'mongodb://localhost/auth'
+);
 
 let connection = mongoose.connection;
 connection.on('connected', function() {
@@ -22,10 +21,15 @@ connection.on('connected', function() {
 });
 
 // ===== server setup =====
-const PORT = process.env.PORT || 3090;
 // const server = http.createServer(app);
 // server.listen(PORT);
 // console.log('Server listening on:', PORT);
+
+// ===== app setup =====
+app.use(morgan('combined')); // logging requests
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -34,17 +38,13 @@ if (process.env.NODE_ENV === "production") {
 
 // ===== Send every request to the React app =====
 // ===== Define any API routes before this runs =====
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
 
 
-// ===== app setup =====
-app.use(morgan('combined')); // logging requests
-app.use(cors());
-app.use(bodyParser.json({ type: '*/*' }));
-router(app);
+
