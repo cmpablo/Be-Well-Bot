ebbb9a65777d414eb1f22d22506c2f17cab9a7a6
cmpablo <christine.pablo@gmail.com>
2018-06-09 21:33:22 -0400

    hid nav buttons unless signed in

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { connect } from 'react-redux';
 
 class Navbar extends Component {
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <div className='mainNav'>
+          <ul>
+            <Link to='/dashboard'>
+            <li><span className="fas fa-list-ul"></span></li>
+            </Link>
+
+            <Link to='/menu'>
+            <li><span className="far fa-smile"></span></li>
+            </Link>
+            
+            <Link to='/logout'>
+            <li><span className="fas fa-sign-out-alt"></span></li>
+            </Link>
+          </ul>
+        </div>
+      );
+    } 
+  }
+
   render() {
     return (
       <div className='mainNav'>
-        <ul>
-          <Link to='/dashboard'>
-          <li><span className="fas fa-list-ul"></span></li>
-          </Link>
-
-          <Link to='/menu'>
-          <li><span className="far fa-smile"></span></li>
-          </Link>
-          
-          <Link to='/logout'>
-          <li><span className="fas fa-sign-out-alt"></span></li>
-          </Link>
-        </ul>
+        {this.renderLinks()}
       </div>
     )
   }
 };
 
-export default Navbar;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Navbar);
